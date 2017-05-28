e776de55692f67d54c035c3de0e872935f1b38ad
Ang Jin Kuan <angjinkuan@hotmail.sg>
2017-05-28 18:47:02 +0800

    Now displays error message when no password

--- run-server.bat
+++ run-server.bat
@@ -1,7 +1,18 @@
+@ECHO OFF
 set pass=%1
 
+if %1.==. goto error
+goto start
+
+:error
+echo "Usage: ./run-server.bat <password>"
+goto end
+
+:start
 cd ./AppServer
 start node ./server.js %pass%
 timeout 3
 cd ../WebServer
 start node ./server.js %pass%
+
+:end
