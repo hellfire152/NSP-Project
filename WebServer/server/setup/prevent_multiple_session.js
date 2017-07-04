67becec21c1473d234fc2ca0fb2b9aba7422ac15
ninggggx99 <qingning99@gmail.com>
2017-07-04 23:31:12 +0800

    trying

---
+++ WebServer/server/setup/prevent_multiple_session.js
@@ -0,0 +1,18 @@
+/*
+  Stores all connections for a current user
+  Will block multiple sessions, from the same user, on the same computer
+*/
+//variable to store socket data
+var connections = {};
+
+module.exports = function checkMultipleOnSameMachine(req, res, next) {
+  //get ip and port
+
+  //IF CONNECTION EXISTS
+    //block connection
+    //remove login cookie
+    //send error page
+  //ELSE (no other session exists)
+    //store into the object
+    //call --> next();
+}
