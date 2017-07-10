9be5ff7d007c2a67c8fe437049f8eed073e5e761
Nigel Chen Chin Hao <nigel.zch@gmail.com>
2017-07-11 03:28:00 +0800

    stupid error solved

--- DatabaseServer/database.js
+++ DatabaseServer/database.js
@@ -323,60 +323,59 @@ var server = net.createServer(function(conn){
                       });
                     }
                     else if(result.length === 0){
-                      console.log(dataOut);
-                      // var query = connection.query("SELECT user_account.user_id, user_account.name, user_account.username, user_account.email, teacher_details.teacher_id, teacher_details.organisation\
-                      //   FROM user_account\
-                      //   LEFT OUTER JOIN teacher_details\
-                      //   ON user_account.user_id = teacher_details.user_id\
-                      //   WHERE teacher_details.user_id = " + connection.escape(dataOut.account.userId),
-                      // function(err, result){
-                      //   if(err){
-                      //     console.error('[Error in query]: ' + err);
-                      //     var response = {
-                      //       data : {
-                      //         success : false,
-                      //         message : err
-                      //       }
-                      //     }
-                      //     sendToServer(response, inputData);
-                      //   }
-                      //   if(result.length === 1){
-                      //     handleDb.handleDecryption(result)
-                      //     .then(resultOut => {
-                      //       objOutResult = {
-                      //         data : resultOut,
-                      //         success : true
-                      //       }
-                      //       sendToServer(objOutResult, inputData);
-                      //     })
-                      //     .catch(reason => {
-                      //       console.log(reason);
-                      //     });
-                      //   }
-                      //   else if(result.length === 0){
-                      //     console.log("[No related data found]");
-                      //     //TODO: Send error message to server
-                      //     var response = {
-                      //       data : {
-                      //         success : false,
-                      //         message : "No such user"
-                      //       }
-                      //     }
-                      //     sendToServer(response, inputData);
-                      //   }
-                      //   else{
-                      //     console.log("[Duplicate user_id, Entity integrity compromise]");
-                      //     //TODO: Send error message to server
-                      //     var response = {
-                      //       data : {
-                      //         success : false,
-                      //         message : "Duplicate user_id"
-                      //       }
-                      //     }
-                      //     sendToServer(response, inputData);
-                      //
-                      //   }
-                      // });
+                      var query = connection.query("SELECT user_account.user_id, user_account.name, user_account.username, user_account.email, teacher_details.teacher_id, teacher_details.organisation\
+                        FROM user_account\
+                        LEFT OUTER JOIN teacher_details\
+                        ON user_account.user_id = teacher_details.user_id\
+                        WHERE teacher_details.user_id = " + connection.escape(dataOut.userId),
+                      function(err, result){
+                        if(err){
+                          console.error('[Error in query]: ' + err);
+                          var response = {
+                            data : {
+                              success : false,
+                              message : err
+                            }
+                          }
+                          sendToServer(response, inputData);
+                        }
+                        if(result.length === 1){
+                          handleDb.handleDecryption(result)
+                          .then(resultOut => {
+                            objOutResult = {
+                              data : resultOut,
+                              success : true
+                            }
+                            sendToServer(objOutResult, inputData);
+                          })
+                          .catch(reason => {
+                            console.log(reason);
+                          });
+                        }
+                        else if(result.length === 0){
+                          console.log("[No related data found]");
+                          //TODO: Send error message to server
+                          var response = {
+                            data : {
+                              success : false,
+                              message : "No such user"
+                            }
+                          }
+                          sendToServer(response, inputData);
+                        }
+                        else{
+                          console.log("[Duplicate user_id, Entity integrity compromise]");
+                          //TODO: Send error message to server
+                          var response = {
+                            data : {
+                              success : false,
+                              message : "Duplicate user_id"
+                            }
+                          }
+                          sendToServer(response, inputData);
+
+                        }
+                      });
                     }
                     else{
                       console.log("[Duplicate user_id, Entity integrity compromise]");
