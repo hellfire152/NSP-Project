daec8b93c6361a01606c1fc354af74941b9c9001
chlololoe <chloeangsl@gmail.com>
2017-07-12 00:26:41 +0800

    email verification workssss

--- WebServer/site/test.js
+++ WebServer/site/test.js
@@ -4,14 +4,15 @@ const xoauth2 = require('xoauth2');
 var transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
-        xoauth2: xoauth2.createXOAuth2Generator({
+      type: 'OAuth2',
             user: 'chloeangsl@gmail.com',
-            clientId: '856574075841-dn1nobjm59p0vrhmvcel4sf4djb6sath.apps.googleusercontent.com',
-            clientSecret: 'mHy11MyVPixj1dx44fX30uoV',
-            refreshToken: '1/3f97hE7yCmipAtuPcu1iu4EhF3kSmzYicMXiamYMjXY'
-        })
-    }
-})
+            clientId: '709561982297-6rtm33rmm3r9gfht1ik8bpgj5j8fglbs.apps.googleusercontent.com',
+            clientSecret: 'ov-S6-aeVkziK7zRbZOfDVjo',
+            refreshToken: '1/eU5uQIN0H0rAe2h8POnA_dzeWjy4-h0JyqjpaH5Gi5RqZlULIHGzA43lUAvKz9jY',
+            accessToken: 'ya29.GluEBDaSLTHtbVkNjdZdwrMjTzybxrovgctjO-AB1ucWRMc6eu24oBJ_a3SKlERX7cKmaIKtc1QyVahUOvV9td1QIZtXLKFPEyZ9uFUCk6vpVGNrpoTMveJgryJu'
+            // accessToken: 'ya29.GluEBEUZ_QQlBXuzlKkBx6VfHqdNo7JBy-ZJm4TQJYedcjtCHnI2GkXrafe5eKdWq4pmBblX0tI3MQdK88VTkyI8VAC2zp60C3_mqLsHLIcCwVJm-WEWhiQASU9P'
+      }
+  })
 
 var mailOptions = {
     from: 'My Name <chloeangsl@gmail.com>',
